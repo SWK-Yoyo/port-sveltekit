df9b1a4b3ca188d0f13729b3c90dc3381d73b09f
Siwakorn Kornnanun <Siwakorn.yoya@gmail.com>
2023-06-22 13:45:29 +0700

    add 2048 game(not finish)

---
+++ src/routes/(auth)/2048-game/+page.svelte
@@ -0,0 +1,46 @@
+<script>
+    let highScore = 0;
+    let tiles = Array(16).fill("");
+
+    function genTile() {
+        console.log(tiles);
+        let randomNum = Math.floor(Math.random() * 2 + 1) * 2;
+        let randomPosition = Math.floor(Math.random() * tiles.length);
+        tiles[randomPosition] = randomNum;
+        console.log(tiles);
+    }
+    function restartGame() {
+        tiles = Array(16).fill(null);
+        genTile();
+        genTile();
+    }
+    restartGame();
+</script>
+
+<section>
+    <div class="frame-2048">
+        {#each tiles as val, inx}
+            <span class="tile">{tiles[inx] ?? ""}</span>
+        {/each}
+    </div>
+    <button> New Game </button>
+</section>
+
+<style>
+    .frame-2048 {
+        display: inline-grid;
+        grid-template-columns: repeat(4, 75px);
+        grid-template-rows: repeat(4, 75px);
+        gap: 0.4rem;
+        background-color: aliceblue;
+        border-radius: 1rem;
+        padding: 1rem;
+    }
+    .tile {
+        background-color: rgb(181, 221, 255);
+        border-radius: 0.3rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
