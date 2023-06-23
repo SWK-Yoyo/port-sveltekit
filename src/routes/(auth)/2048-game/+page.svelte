d7dc4d504e5dec2d7611386cfab26d46ae36b66b
Yoya Siwakorn <>
2023-06-23 12:23:07 +0700

    2048 update

--- src/routes/(auth)/2048-game/+page.svelte
+++ src/routes/(auth)/2048-game/+page.svelte
@@ -14,16 +14,39 @@
         genTile();
         genTile();
     }
+
+    function moveTile() {
+        for (let i = 0; i < 4; i++) {
+            let prevPosition = 0;
+            let prevVal = "";
+            // let position = 0;
+            for (let x = 0; x < 4; x++) {
+                let position = i + x;
+                let tile = tiles[position];
+                if (prevVal) {
+                    if(prevVal === tile){
+                        tiles[prevPosition] = tile + tile
+                        
+                    }
+                } else if (tile) {
+                    prevVal = tile
+                    prevPosition = position
+                }
+            }
+        }
+    }
+
     restartGame();
 </script>
 
+<svelte:window on:keyup={moveTile} />
 <section>
     <div class="frame-2048">
         {#each tiles as val, inx}
             <span class="tile">{tiles[inx] ?? ""}</span>
         {/each}
     </div>
-    <button> New Game </button>
+    <button on:click={restartGame}> New Game </button>
 </section>
 
 <style>
