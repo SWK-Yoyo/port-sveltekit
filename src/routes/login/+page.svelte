4cc701c7d47291174fe12a8e8b3b2fd9b70e4a6f
Siwakorn Kornnanun <76690316+Yoya96@users.noreply.github.com>
2024-08-24 22:22:51 +0700

    Day 10

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -2,35 +2,34 @@
     import InputText from "../../lib/form-component/InputText.svelte";
     import { submitForm, usersPattern } from "../../lib/form.js";
     import { PUBLIC_API_URL } from "$env/static/public";
-
+    import { user } from "../../lib/user.js";
     import Box from "../../lib/Box.svelte";
     import BoxError from "../../lib/BoxError.svelte";
-    export let data;
 
-    let form = {
-        status: true,
+    let error = {
+        status: false,
         message: "",
     };
+
     let params = {
-        username: {
-            value: "",
-            status: true,
-        },
-        password: {
-            value: "",
-            status: true,
-        },
+        username: "",
+        password: "",
     };
 
     async function onSubmit(e) {
-        form.status = true;
+        error.status = false;
         e.preventDefault();
-        let response = await submitForm(e, params);
-        if (response) {
-            form.status = response.status;
-            form.message = response.message;
-            form = form;
+        const response = await submitForm(e, params);
+        if (!response.status) {
+            error = {
+                status: true,
+                message: response.message,
+            };
+            error = error;
+            return;
         }
+
+        window.location.href = "/";
     }
 </script>
 
@@ -43,14 +42,14 @@
 >
     <Box>
         <h1>Sign in</h1>
-        <BoxError show={!form.status}>{form.message}</BoxError>
+        <BoxError show={error.status}>{error.message}</BoxError>
         <InputText
             pattern={$usersPattern.username.regex}
             name="username"
             placeholder="Username"
             errorText="test tes"
             required={true}
-            bind:value={params.username.value}
+            bind:value={params.username}
         ></InputText>
         <InputText
             type="password"
@@ -58,8 +57,8 @@
             name="password"
             placeholder="Password"
             required={true}
-            bind:value={params.password.value}
-            ></InputText>
+            bind:value={params.password}
+        ></InputText>
         <div class="button-wrapper">
             <a href="/register">Not have an account</a>
             <button>Sign in</button>
